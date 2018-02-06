8501a4655e70b912aee23dcfe7016cf59e387ff7
Philipe Fatio <me@phili.pe>
2018-02-06 07:46:59 +0100

    Handle ports properly
    Fixes #3
    Closes #4

--- lib/xipio/middleware.rb
+++ lib/xipio/middleware.rb
@@ -5,7 +5,7 @@ module Xipio
     end
 
     def call(env)
-      tld_length = env['HTTP_HOST'].end_with?('.xip.io') ? 6 : 1
+      tld_length = env['HTTP_HOST'].match?(/\.xip\.io(:\d{1,5})?\z/) ? 6 : 1
       ActionDispatch::Http::URL.tld_length = tld_length
       @app.call(env)
     end
